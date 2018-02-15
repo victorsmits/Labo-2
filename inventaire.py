863167c1c4a114aca068c78091eb4dd97f538d3a
victorsmits <victor.smits@student.vinci.be>
2018-02-15 10:32:09 +0100

    inventaire update

--- inventaire.py
+++ inventaire.py
@@ -1,14 +1,31 @@
 import re
+import sys
 
 
 def inventaire(path):
     n = 1
+    finde=''
     with open(path, 'r') as file:
-        number = re.compile(r'-?[1-9][0-9]*')
+        number = re.compile(r'\d+')
         line = file.readlines()
         for i in line:
-            if number.match(str(i)) is not None:
-                print('line {}: {}'.format(n,i))
+            content = number.findall(i)
+            if len(content) != 0:
+                for txt in content:
+                    if len(content) >1:
+                        finde += '{}, '.format(txt)
+                    else:
+                        finde += '{}'.format(txt)
+                print('line {}: {}'.format(n, finde))
+                finde = ''
             n += 1
 
+def path():
+    file = re.compile(r'-[a-z]*\.txt$')
+    sys = sys.arg
+    for i in sys:
+        if file.match(i) is not None:
+            return i
+#txt = path()
+#inventaire(txt)
 inventaire('inventaire_test.txt')
