24dd9fdefa2c0f5a94d7d2b700cc935a6f6446c9
victorsmits <victor.smits@student.vinci.be>
2018-02-25 17:37:33 +0100

    cross word end

--- cross_word.py
+++ cross_word.py
@@ -2,26 +2,38 @@ import re
 
 linesregex = ['\d', '[a-z]', '[A-Z]']
 columnsregex = ['[1-9]', '\d+', '[a-z]']
-answer_line = ['1bc', 'bcd', 'CDE']
+answer_line = ['1bc', 'def', 'HIJ']
 
 
 def checkregexcrossword(linesregex, columnsregex, answer):
     assert (len(linesregex) == len(answer))
-    n = 0
     x = 0
-    while n < len(answer):
-        for regex_lines in linesregex:
-            pattern_lines = re.compile(regex_lines)
-            state_line = pattern_lines.match(answer[n]) is not None
-            # print(state_line)
-        n += 1
-
-    while x < len(columnsregex):
-        pattern_col = re.compile(columnsregex[x])
-        for letter in answer[x]:
-            state_col = pattern_col.match(letter) is not None
-            print(pattern_col, letter, state_col)
+    z = 0
+    col = {}
+    line = {}
+    while x < len(linesregex):
+        y = x+1
+        if y in line:
+            line[y] = ''
+        line[y] = answer[x]
+        pattern_lines = re.compile(linesregex[x])
+        state_line = pattern_lines.match(line[y]) is not None
+        print('Line', linesregex[x], ':', line[y], ':', state_line)
         x += 1
+
+    while z < len(columnsregex):
+        y = z + 1
+        letter_list = []
+        for letter in answer:
+            letter_list.append(letter[z])
+        if y in col:
+            col[y] = ''
+        col[y] = ''.join(letter_list)
+        pattern_col = re.compile(columnsregex[z])
+        state_col = pattern_col.match(col[y]) is not None
+        print('Column', columnsregex[z], ':', col[y], ':', state_col)
+        z += 1
+
 
 
 checkregexcrossword(linesregex, columnsregex, answer_line)
